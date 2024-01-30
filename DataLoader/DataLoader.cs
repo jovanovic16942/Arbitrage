1e39ed0af56510e9b8b31531231a478be3ed339b
Filip Jovanovic <jovanovic16942@gmail.com>
2024-01-30 18:25:57 +0100

    Updated DataLoader to handle multiple sports better

--- DataLoader/DataLoader.cs
+++ DataLoader/DataLoader.cs
@@ -1,50 +1,99 @@
 ﻿using Arbitrage.General;
 using Arbitrage.Utils;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using NLog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Arbitrage.DataLoader
 {
     internal class DataLoader
     {
-        List<HouseMatchData>? _data;
+        Dictionary<Sport, List<HouseMatchData>> _data;
         readonly IParser _parser;
 
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static Logger log = LogManager.GetCurrentClassLogger();
 
         public DataLoader(BettingHouse house)
         {
             _parser = ParserFactory.GetParser(house);
-            logger.Info("Created DataLoader for {@house}", house);
+
+            _data = new();
+
+            foreach (Sport sport in Enum.GetValues(typeof(Sport)))
+            {
+                _data.Add(sport, new List<HouseMatchData>());
+            }
+
+            log.Info("Created DataLoader for {@house}", house);
         }
 
         public List<HouseMatchData> GetData()
         {
-            if (!_data.IsNullOrEmpty())
+            if (_data.IsNullOrEmpty())
             {
-                return _data!;
+                log.Error("Data is null or empty!");
+                return new();
             }
 
-            logger.Error("Data is null or empty!");
-            return new();
+            List<HouseMatchData> data = new();
+
+            foreach (var d in _data.Values) 
+            {
+                data.AddRange(d);
+            }
+
+            return data;
+
         }
 
-        public async Task Load(Sport sport) {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sports">List of sports to load</param>
+        /// <param name="p">Load all sports in parallel if true</param>
+        /// <returns></returns>
+        public async Task Load(List<Sport> sports = null, bool p = false) {
+
+            if (sports is null)
+            {
+                var values = Enum.GetValues(typeof(Sport));
+                sports = new List<Sport>((Sport[])values);
+            }
+
             Console.WriteLine(_parser.GetName() + " download started...");
-            await UpdateData(sport);
+            await UpdateData(sports, p);
             Console.WriteLine(_parser.GetName() + " download complete");
         }
 
-        private async Task UpdateData(Sport sport)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sports">List of sports to load</param>
+        /// <param name="p">Load all sports in parallel if true</param>
+        /// <returns></returns>
+        private async Task UpdateData(List<Sport> sports, bool p = false)
         {
-            _data = _parser.ParseSport(sport);
-            // TODO update database
+            List<Sport> _s = new();
+
+            if (p)
+            {
+                Parallel.ForEach(sports, sport =>
+                {
+                    _ = UpdateSport(sport);
+                });
+            } else
+            {
+                foreach (var sport in sports)
+                {
+                    _ = UpdateSport(sport);
+                }
+            }
+        }
+
+        private async Task UpdateSport(Sport sport)
+        {
+            Console.WriteLine(_parser.GetName() + " " + sport + " download started...");
+            _data[sport] = _parser.ParseSport(sport);
+            Console.WriteLine(_parser.GetName() + " " + sport + " download complete");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,21 +15,21 @@ List <DataLoader> dataLoaders = new()
     //new DataLoader(new StarBetParser()),
     //new DataLoader(new OlimpParser()),
     //new DataLoader(new Bet365Parser()),
-    new DataLoader(BettingHouse.Meridian),
-    //new DataLoader(BettingHouse.PinnBet),
-    //new DataLoader(BettingHouse.Mozzart),
-    //new DataLoader(BettingHouse.AdmiralBet),
-    //new DataLoader(BettingHouse.SuperBet),
-    //new DataLoader(BettingHouse.MerkurXTip),
-    //new DataLoader(BettingHouse.MaxBet),
-    //new DataLoader(BettingHouse.OktagonBet),
-    //new DataLoader(BettingHouse.BetOle),
+    new DataLoader(BettingHouse.Meridian), // IN PROGRESS
+    new DataLoader(BettingHouse.PinnBet),
+    new DataLoader(BettingHouse.Mozzart),
+    new DataLoader(BettingHouse.AdmiralBet),
+    new DataLoader(BettingHouse.SuperBet),
+    new DataLoader(BettingHouse.MerkurXTip),
+    new DataLoader(BettingHouse.MaxBet),
+    new DataLoader(BettingHouse.OktagonBet),
+    new DataLoader(BettingHouse.BetOle),
 };
 
 // Load the data in parallel
 Parallel.ForEach(dataLoaders, loader =>
 {
-    _ = loader.Load(Sport.Football);
+    _ = loader.Load();
 });
 
 // Match data from different sources
